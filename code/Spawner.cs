9227c4ac26fd3093aca6c92d4b1ee54112c6ffe4
ryleigh <ryleighkostash@gmail.com>
2024-01-04 23:34:56 -0800

    spawner tweaks

--- code/Spawner.cs
+++ code/Spawner.cs
@@ -1,9 +1,22 @@
 using Sandbox;
+using System.Linq;
 
 public sealed class Spawner : Component
 {
+	private GameObject _ring;
+	private GameObject _bgLight;
+
+	protected override void OnEnabled()
+	{
+		base.OnEnabled();
+
+		_ring = GameObject.Children.Where( x => x.Name == "ring" ).FirstOrDefault();
+		_bgLight = GameObject.Children.Where( x => x.Name == "bg_light" ).FirstOrDefault();
+	}
+
 	protected override void OnUpdate()
 	{
-
+		_ring.Transform.LocalRotation = Rotation.FromRoll( Time.Now * 30f );
+		_bgLight.Transform.LocalRotation = Rotation.FromRoll( -Time.Now * 25f );
 	}
 }
